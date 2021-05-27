8be6caf026fe161bb080e8f8ef130de5383db8c1
ogreks <ogreks@hotmail.com>
2021-05-27 10:16:47 +0800

    [style] 删除原先vendor , 新增composer.lock
    降低仓库大小、使用composer.lock 控制版本

--- vendor/swoft/http-message/test/bean.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'logger'            => [
-        'flushRequest' => false,
-        'enable'       => false,
-        'json'         => false,
-    ],
-];
--- vendor/swoft/process/test/bean.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'logger'            => [
-        'flushRequest' => false,
-        'enable'       => false,
-        'json'         => false,
-    ],
-];
